2bb211d8ba1741ccb594d054aa43c0ec3ebab94c
ktoori <crug.vika@gmail.com>
2024-06-06 23:44:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+def add(a, b):
+    """Возвращает сумму a и b."""
+    return a + b
+
+
+def subtract(a, b):
+    """Возвращает разницу между a и b."""
+    return a - b
+
+
+def multiply(a, b):
+    """Возвращает произведение a и b."""
+    return a * b
+
+
+def divide(a, b):
+    """Возвращает частное от деления a на b."""
+    if b != 0:
+        return a / b
+    else:
+        return "На ноль делить нельзя!"
+
+
+def calculator(a, b, operator):
+    """Основная функция, которая демонстрирует работу арифметических операций."""
+    if operator == '+':
+        return add(a, b)
+    elif operator == '-':
+        return subtract(a, b)
+    elif operator == '*':
+        return multiply(a, b)
+    elif operator == '/':
+        return divide(a, b)
+    else:
+        return -1
