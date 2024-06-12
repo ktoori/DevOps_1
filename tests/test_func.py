5c9acee29efd42f00c08fc975ea1822e6d705384
Vika Kruglova <152300646+ktoori@users.noreply.github.com>
2024-06-12 18:16:37 +0300

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -2,19 +2,22 @@ import pytest
 
 from main import calculator, add, subtract, multiply, divide
 
+
 @pytest.mark.parametrize(
-    ("x", "y", "res"), [
+    ("x", "y", "res"), 
+    [
         (10, 5, 15),
         (2.5, 1.5, 4),
         (0, 0, 0),
     ]
 )
-
 def test_add(x, y, res):
     assert add(x, y) == res
 
+
 @pytest.mark.parametrize(
-    ("x", "y", "res"), [
+    ("x", "y", "res"), 
+    [
         (10, 5, 5),
         (3, 5, -2),
         (0, 0, 0),
@@ -23,8 +26,10 @@ def test_add(x, y, res):
 def test_sub(x, y, res):
     assert subtract(x, y) == res
 
+
 @pytest.mark.parametrize(
-    ("x", "y", "res"), [
+    ("x", "y", "res"), 
+    [
         (10, 5, 50),
         (0, 5, 0),
         (5, -1, -5),
@@ -33,8 +38,10 @@ def test_sub(x, y, res):
 def test_mult(x, y, res):
     assert multiply(x, y) == res
 
+
 @pytest.mark.parametrize(
-    ("x", "y", "res"), [
+    ("x", "y", "res"), 
+    [
         (10, 5, 2),
         (0, 5, 0),
         (5, 0, -1),
@@ -43,8 +50,10 @@ def test_mult(x, y, res):
 def test_div(x, y, res):
     assert divide(x, y) == res
 
+
 @pytest.mark.parametrize(
-    ("x", "y", "op", "res"), [
+    ("x", "y", "op", "res"), 
+    [
         (10, 5, "+", 15),
         (10, 5, "-", 5),
         (10, 5, "*", 50),
